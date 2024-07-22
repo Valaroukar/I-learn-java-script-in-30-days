532bf454aa0f9c8f5cf71f1326b2a95a2ca99fac
Serhan Üst <ustserhan@gmail.com>
2024-07-22 12:37:50 +0300

    Day 1
    Day 1

---
+++ Day 1/introduction.js
@@ -0,0 +1,16 @@
+console.log(typeof'filmolog')
+console.log(typeof 5)
+console.log(typeof 5.5)
+console.log(typeof true )
+console.log(typeof null)
+console.log(typeof undefined)
+
+let youtubeChannel ='Filmolog'
+let channelAge = 1
+let secondChannel ="null"
+let moneyChannel = undefined
+
+const GRAVITY = 9.81
+const PI = 3.14
+
+console.log (GRAVITY,youtubeChannel, channelAge, isChannelStillActive, secondChannel)
